5cf5462c4e59c26556264731071d53c002464647
LindaMangena <lindajazz42@gmail.com>
2022-02-14 20:02:54 +0200

    Project complete

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ const success    = document.querySelector("#success");
 const errorNodes = document.querySelectorAll(".error");
 
 // Validate data
-function validateForm() {
+    function validateForm() {
 
     clearMessages();
+
+    let errorFlag = false;
+
+
 
     if (nameInput.value.length < 1) {
         errorNodes[0].innerText = "Name cannot be blank";
         nameInput.classList.add("error-border");
+        errorFlag = true;
     }
 
     if (!emailIsValid(email.value)) {
@@ -25,18 +30,25 @@ function validateForm() {
     if (message.value.length < 1) {
         errorNodes[2].innerText = "please enter message ";
         message.classList.add("error-border");
+        errorFlag = true;
 
+    }
+
+    if(!errorFlag){
+        success.innerText = "Success";
     }
 }
 
 // clear error / success messages
-function clearMessages() {
+    function clearMessages() {
 
     for (let i = 0; i < errorNodes.length; i++) {
-        errorNodes[1].innerText = "Invalid email address";
+        errorNodes[1].innerText = "";  
     }
 
-    nameInput.classList.remove("error");
+    nameInput.classList.remove("error-border");
+    message.classList.remove("error-border");
+
 
 }
 // check if the email is valid
